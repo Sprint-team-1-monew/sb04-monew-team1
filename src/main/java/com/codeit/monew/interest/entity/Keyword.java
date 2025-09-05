7d28311f61746a90760db4ef0f7b39f9a85e0d0a
Eunhye0k <dmsgur7305@naver.com>
2025-09-05 10:26:12 +0900

    feat(댓글 관리): Comment create 기능 구현 완료, 각 Entity @Builder 를 @SuperBuilder로 변경 및 추가

--- src/main/java/com/codeit/monew/interest/entity/Keyword.java
+++ src/main/java/com/codeit/monew/interest/entity/Keyword.java
@@ -9,6 +9,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
