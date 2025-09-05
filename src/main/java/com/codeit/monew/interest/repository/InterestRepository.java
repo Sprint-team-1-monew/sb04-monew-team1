4b5f6fef580807d55bbe3a05c3ac241361340e25
KwonYongJin <chaoskyj@gmail.com>
2025-09-05 15:31:50 +0900

    feat레포지터리 추가

---
+++ src/main/java/com/codeit/monew/interest/repository/InterestRepository.java
@@ -0,0 +1,11 @@
+package com.codeit.monew.interest.repository;
+
+import com.codeit.monew.article.entity.Article;
+import com.codeit.monew.interest.entity.Interest;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestRepository extends JpaRepository<Interest, UUID> {
+
+}
---
+++ src/main/java/com/codeit/monew/interest/repository/KeywordRepository.java
@@ -0,0 +1,15 @@
+package com.codeit.monew.interest.repository;
+
+import com.codeit.monew.article.entity.Article;
+import com.codeit.monew.interest.entity.Interest;
+import com.codeit.monew.interest.entity.Keyword;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value.Int;
+
+public interface KeywordRepository extends JpaRepository<Keyword, UUID> {
+
+  List<Keyword> findByInterest(Interest interest); // 기사 쪽에서 쓰임
+}
