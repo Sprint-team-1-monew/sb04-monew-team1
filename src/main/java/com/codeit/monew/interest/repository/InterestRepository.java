314e70421da805a70cdd031335e50cc0ea7c54bd
Oh-Myeongjae <omjcws@gmail.com>
2025-09-05 14:32:18 +0900

    feat(관심사 관리): 관심사 등록 구현
    - 관심사 컨트롤러 구현
    - 관심사 서비스 구현
    - 관심사 레포지토리 구현

---
+++ src/main/java/com/codeit/monew/interest/repository/InterestRepository.java
@@ -0,0 +1,16 @@
+package com.codeit.monew.interest.repository;
+
+import com.codeit.monew.interest.entity.Interest;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InterestRepository extends JpaRepository<Interest, UUID> {
+
+  List<Interest> findAllByDeletedAtFalse();
+
+  Optional<Interest> findByIdAndDeletedAtFalse(UUID id);
+}
