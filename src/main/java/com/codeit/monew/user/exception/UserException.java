8c7297a1315c567f751a35f1983361f793b023c2
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-09-06 17:00:49 +0900

    reafactor(사용자 관리): 리뷰 내용 반영
    - 컨트롤러 이름 오타 수정
    - 커스텀 예외 도메인 이름 단위로 통합

--- src/main/java/com/codeit/monew/user/exception/UserException.java
+++ src/main/java/com/codeit/monew/user/exception/UserException.java
@@ -5,8 +5,8 @@ import com.codeit.monew.exception.global.MoNewException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
-public class UserEmailDuplicatedException extends MoNewException {
-  public UserEmailDuplicatedException(ErrorCode errorCode, String email) {
-    super(LocalDateTime.now(), errorCode, Map.of("email", email));
+public class UserException extends MoNewException {
+  public UserException(ErrorCode errorCode, Map<String, Object> details) {
+    super(LocalDateTime.now(), errorCode, details);
   }
 }
