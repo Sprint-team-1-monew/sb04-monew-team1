af8c474bdb4b9369aa5226f7ee9485d2d19847cf
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-09-06 17:00:49 +0900

    reafactor(사용자 관리): 리뷰 내용 반영
    - 컨트롤러 이름 오타 수정
    - 커스텀 예외 도메인 이름 단위로 통합

--- src/main/java/com/codeit/monew/user/controller/UserController.java
+++ src/main/java/com/codeit/monew/user/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class UseController {
+public class UserController {
 
   private final UserService userService;
 
--- src/main/java/com/codeit/monew/user/service/UserService.java
+++ src/main/java/com/codeit/monew/user/service/UserService.java
@@ -2,14 +2,14 @@ package com.codeit.monew.user.service;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.codeit.monew.user.entity.User;
-import com.codeit.monew.user.exception.UserEmailDuplicatedException;
 import com.codeit.monew.user.exception.UserErrorCode;
-import com.codeit.monew.user.exception.UserNotExistException;
+import com.codeit.monew.user.exception.UserException;
 import com.codeit.monew.user.mapper.UserMapper;
 import com.codeit.monew.user.repository.UserRepository;
 import com.codeit.monew.user.request.UserRegisterRequest;
 import com.codeit.monew.user.request.UserUpdateRequest;
 import com.codeit.monew.user.response_dto.UserDto;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -38,17 +38,15 @@ public class UserService {
 
   public UserDto updateUser(UUID userId, UserUpdateRequest userUpdateRequest) {
     User user = userRepository.findById(userId)
-        .orElseThrow(() -> new UserNotExistException(UserErrorCode.USER_NOT_EXIST, userId));
+        .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_EXIST, Map.of("userId", userId)));
 
     Optional.ofNullable(userUpdateRequest.nickname()).ifPresent(user::updateNickname);
     return userMapper.toDto(userRepository.save(user));
   }
 
-
-
   private void validateEmailDoesNotExist(String email) {
     if (userRepository.existsByEmail(email)) {
-      throw new UserEmailDuplicatedException(UserErrorCode.USER_EMAIL_DUPLICATED, email);
+      throw new UserException(UserErrorCode.USER_EMAIL_DUPLICATED, Map.of("email", email));
     }
   }
 
