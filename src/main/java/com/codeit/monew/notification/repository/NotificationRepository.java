8ee431da122a2263e169557da685c6d0b854a9ca
hochan <106953872+cheis11@users.noreply.github.com>
2025-09-08 16:04:18 +0900

    feat(알림 관리): 알림 전체 확인
    Service에서 사용자의 모든 알림을 한 번에 확인하는 로직 작성 및 성공 테스트 작성

--- src/main/java/com/codeit/monew/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/monew/notification/repository/NotificationRepository.java
@@ -1,8 +1,12 @@
 package com.codeit.monew.notification.repository;
 
 import com.codeit.monew.notification.entity.Notification;
+import com.codeit.monew.user.entity.User;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+  List<Notification> findByUserAndConfirmedFalse(User user);
 }
