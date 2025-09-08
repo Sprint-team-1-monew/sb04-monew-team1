87d0f5a6af5bae011e1955ab63ad0ab33abcfcdb
Eunhye0k <dmsgur7305@naver.com>
2025-09-08 11:08:28 +0900

    refactor(댓글 관리): UserRepository와 ArticleRepository 연동으로 인한 CommentService 코드 수정

--- src/main/java/com/codeit/monew/comment/service/CommentService.java
+++ src/main/java/com/codeit/monew/comment/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.codeit.monew.comment.service;
 
+import com.codeit.monew.article.repository.ArticleRepository;
 import com.codeit.monew.comment.entity.Comment;
 import com.codeit.monew.comment.mapper.CommentMapper;
 import com.codeit.monew.comment.repository.CommentRepository;
@@ -7,6 +8,8 @@ import com.codeit.monew.comment.request.CommentRegisterRequest;
 import com.codeit.monew.comment.request.CommentUpdateRequest;
 import com.codeit.monew.comment.response_dto.CommentDto;
 import com.codeit.monew.exception.comment.CommentNotFoundException;
+import com.codeit.monew.user.repository.UserRepository;
+import com.codeit.monew.user.service.UserService;
 import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -22,29 +25,21 @@ public class CommentService {
 
   private final CommentRepository commentRepository;
   private final CommentMapper commentMapper;
+  private final UserRepository userRepository;
+  private final ArticleRepository articleRepository;
 
   public CommentDto createComment(CommentRegisterRequest commentRegisterRequest) {
 
     Comment commentEntity = commentMapper.toCommentEntity(commentRegisterRequest);
 
-    //userRepository 연동되면 삭제
-    if (!userExists(commentEntity.getUser().getId())) {
+    if(!userRepository.existsById(commentEntity.getUser().getId())) {
       throw new IllegalArgumentException("사용자 ID가 존재하지 않습니다.");
     }
-    //articleRepository 연동되면 삭제
-    if (!articleExists(commentEntity.getArticle().getId())) {
+
+    if(!articleRepository.existsById(commentEntity.getArticle().getId())) {
       throw new IllegalArgumentException("기사가 존재하지 않습니다.");
     }
 
-    //사용자 존재 확인
-//    if(!userRepository.existsById(commentEntity.getUser().getId())) {
-//      throw new IllegalArgumentException("사용자 ID가 존재하지 않습니다.");
-//    }
-    //기사 존재 확인
-//    if(!articleRepository.existsById(commentEntity.getArticle().getId())) {
-//      throw new IllegalArgumentException("기사가 존재하지 않습니다.");
-//    }
-    //저장
 
     try {
       Comment saved = commentRepository.save(commentEntity);
@@ -55,16 +50,6 @@ public class CommentService {
       log.error("댓글 저장 중 오류 발생: {}", e.getMessage());
       throw e;
     }
-  }
-
-  private boolean userExists(UUID userId) {
-    // TODO: userRepository 연결되면 실제 체크
-    return true;
-  }
-
-  private boolean articleExists(UUID articleId) {
-    // TODO: articleRepository 연결되면 실제 체크
-    return true;
   }
 
   public CommentDto updateComment(UUID commentId, UUID userId,
