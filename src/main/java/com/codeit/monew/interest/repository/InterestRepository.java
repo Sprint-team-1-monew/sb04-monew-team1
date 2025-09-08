f7f829e5def89c719464a0eae11d8fa8391b0dab
Oh-Myeongjae <omjcws@gmail.com>
2025-09-08 16:04:08 +0900

    feat(관심사 관리): 관심사 목록 조회 구현

--- src/main/java/com/codeit/monew/interest/repository/InterestRepository.java
+++ src/main/java/com/codeit/monew/interest/repository/InterestRepository.java
@@ -1,10 +1,15 @@
 package com.codeit.monew.interest.repository;
 
-import com.codeit.monew.article.entity.Article;
 import com.codeit.monew.interest.entity.Interest;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface InterestRepository extends JpaRepository<Interest, UUID> {
+@Repository
+public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestRepositoryCustom{
+
+  List<Interest> findAllByDeletedAtFalse();
 
 }
+
--- src/main/java/com/codeit/monew/interest/repository/KeywordRepository.java
+++ src/main/java/com/codeit/monew/interest/repository/KeywordRepository.java
@@ -1,15 +1,17 @@
 package com.codeit.monew.interest.repository;
 
-import com.codeit.monew.article.entity.Article;
 import com.codeit.monew.interest.entity.Interest;
 import com.codeit.monew.interest.entity.Keyword;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value.Int;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KeywordRepository extends JpaRepository<Keyword, UUID> {
-
+  
   List<Keyword> findByInterest(Interest interest); // 기사 쪽에서 쓰임
+
+  List<Keyword> findAllByInterest_IdAndDeletedAtFalse(UUID interestId);
 }
+
