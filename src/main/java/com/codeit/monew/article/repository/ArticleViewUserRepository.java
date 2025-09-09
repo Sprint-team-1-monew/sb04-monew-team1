43ab13dc60a28bb4d72f14ddcd71d4febe9a1e7f
KwonYongJin <chaoskyj@gmail.com>
2025-09-09 15:49:49 +0900

    feat(뉴스 기사 관리): 기사 뷰 등록
    기사 뷰 등록 코드 구현 및 테스트 코드 구현

--- src/main/java/com/codeit/monew/article/repository/ArticleViewUserRepository.java
+++ src/main/java/com/codeit/monew/article/repository/ArticleViewUserRepository.java
@@ -2,7 +2,6 @@ package com.codeit.monew.article.repository;
 
 import com.codeit.monew.article.entity.Article;
 import com.codeit.monew.article.entity.ArticlesViewUser;
-import com.codeit.monew.interest.entity.Interest;
 import com.codeit.monew.user.entity.User;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArticleViewUserRepository extends JpaRepository<ArticlesViewUser, UUID> {
   boolean existsByArticleAndUser(Article article, User user);
+  Optional<ArticlesViewUser> findByArticleAndUser(Article article, User user);
 }
