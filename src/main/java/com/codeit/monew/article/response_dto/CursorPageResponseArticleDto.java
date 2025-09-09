7cde779b87c14abde0dbf54f257d8b1b08c1dac5
KwonYongJin <chaoskyj@gmail.com>
2025-09-09 10:32:09 +0900

    feat(뉴스기사관리): 뉴스 기사 목록 조회 코드 작성
    뉴스 기사 목록 조회 코드 작성

--- src/main/java/com/codeit/monew/article/response_dto/CursorPageResponseArticleDto.java
+++ src/main/java/com/codeit/monew/article/response_dto/CursorPageResponseArticleDto.java
@@ -2,9 +2,10 @@ package com.codeit.monew.article.response_dto;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public record CursorPageResponseArticleDto(
-  ArrayList<ArticleDto> content,
+  List<ArticleDto> content,
   String nextCursor,
   LocalDateTime nextAfter,
   int size,
