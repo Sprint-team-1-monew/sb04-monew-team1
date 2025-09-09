689e39341fe705d48e2af9ef69259299a252401c
hochan <106953872+cheis11@users.noreply.github.com>
2025-09-09 15:55:03 +0900

    feat(알림 관리): 알림 삭제
    confirm이 true인 알림 중 updatedAt이 지금 시간으로부터 일주일 전이면 배치와 스케줄러를 이용해 자동으로 삭제

---
+++ src/main/java/com/codeit/monew/notification/batch/NotificationCleanupScheduler.java
@@ -0,0 +1,27 @@
+package com.codeit.monew.notification.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NotificationCleanupScheduler {
+
+  private final JobLauncher jobLauncher;
+  private final Job notificationCleanupJob;
+
+  @Scheduled(cron = "${spring.batch.notification-cleanup.cron}")
+  public void runJob() throws Exception {
+    JobParameters jobParameters = new JobParametersBuilder()
+        .addLong("time", System.currentTimeMillis())
+        .toJobParameters();
+    jobLauncher.run(notificationCleanupJob, jobParameters);
+  }
+}
--- src/main/java/com/codeit/monew/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/monew/notification/repository/NotificationRepository.java
@@ -2,11 +2,25 @@ package com.codeit.monew.notification.repository;
 
 import com.codeit.monew.notification.entity.Notification;
 import com.codeit.monew.user.entity.User;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
   List<Notification> findByUserAndConfirmedFalse(User user);
+
+  List<Notification> findByConfirmedTrueAndUpdatedAtBefore(LocalDateTime weekAgo);
+
+  //테스트용
+  @Modifying
+  @Transactional // update 쿼리 실행에 필요한 트랜잭션
+  @Query("update Notification n set n.updatedAt = :updatedAt where n.id = :id")
+  void updateUpdatedAt(@Param("id") UUID id, @Param("updatedAt") LocalDateTime updatedAt);
 }
+
