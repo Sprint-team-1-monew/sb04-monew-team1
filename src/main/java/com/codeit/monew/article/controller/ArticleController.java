586185494bae73b60d45b021b1293ceb9af0e17c
KwonYongJin <chaoskyj@gmail.com>
2025-09-09 14:31:22 +0900

    feat(뉴스기사관리): 글로벌 폴더 생생
    글로벌 폴더 생생 및 config 클래스 생성

--- src/main/java/com/codeit/monew/article/controller/ArticleController.java
+++ src/main/java/com/codeit/monew/article/controller/ArticleController.java
@@ -10,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
