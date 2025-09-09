5e6dcc775236dbea317b15c36f2c95e155c0567f
KwonYongJin <chaoskyj@gmail.com>
2025-09-09 14:04:49 +0900

    fix(뉴스기사관리): ArticleController 어노테이션 수정
    Controller 어노테이션으로 작성되어있어서 RestController로 수정

--- src/main/java/com/codeit/monew/article/controller/ArticleController.java
+++ src/main/java/com/codeit/monew/article/controller/ArticleController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/articles")
 public class ArticleController {
