dbd620469bda23d6fb3b9cd4e82351edd1d8a31f
KwonYongJin <chaoskyj@gmail.com>
2025-09-09 10:32:09 +0900

    feat(뉴스기사관리): 뉴스 기사 목록 조회 코드 작성
    뉴스 기사 목록 조회 코드 작성

--- src/main/java/com/codeit/monew/article/service/ArticleService.java
+++ src/main/java/com/codeit/monew/article/service/ArticleService.java
@@ -1,30 +1,39 @@
 package com.codeit.monew.article.service;
 
 import com.codeit.monew.article.entity.Article;
+import com.codeit.monew.article.mapper.ArticleMapper;
 import com.codeit.monew.article.repository.ArticleRepository;
+import com.codeit.monew.article.repository.ArticleViewUserRepository;
+import com.codeit.monew.article.response_dto.ArticleDto;
 import com.codeit.monew.article.response_dto.CursorPageResponseArticleDto;
 import com.codeit.monew.exception.article.ArticleException;
 import com.codeit.monew.exception.article.ArticleNotFoundException;
+import com.codeit.monew.user.entity.User;
+import com.codeit.monew.user.repository.UserRepository;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class ArticleService {
 
   private final ArticleRepository articleRepository;
-
-  public ArticleService(ArticleRepository articleRepository) {
-    this.articleRepository = articleRepository;
-  }
+  private final ArticleMapper articleMapper;
+  private final ArticleViewUserRepository articleViewUserRepository;
+  private final UserRepository userRepository;
 
   public void softDeleteArticle(UUID articleId) {
     log.info("기사 논리 삭제 시작 {}", articleId);
@@ -57,10 +66,42 @@ public class ArticleService {
     log.info("뉴스 기사 검색 시작 - keyword={}, interestId={}, sourceIn={}, publishDateFrom={}, publishDateTo={}, orderBy={}, direction={}, cursor={}, after={}, limit={}, requestUserId={}",
         keyword, interestId, sourceIn, publishDateFrom, publishDateTo, orderBy, direction, cursor, after, limit, requestUserId);
 
+    List<Article> articles = articleRepository.searchArticles(keyword, interestId, sourceIn, publishDateFrom, publishDateTo, orderBy, direction, cursor, after, limit);
+    List<ArticleDto> content = null;
+    Optional<User> user = userRepository.findById(requestUserId); // npe 날 수 있으니 예외 처리, 충돌을 우려하며 나중에 유저 쪽 코드랑 머지 되었을 때 예외 처리 추가
 
+    if (articles.size() > limit) {
+      articles = articles.subList(0, limit);
+    }
+
+    for (Article article : articles) {
+      boolean viewed = articleViewUserRepository.existsByArticleAndUser(article, user.get());
+      ArticleDto dto = articleMapper.toDto(article, viewed);
+      content.add(dto);
+    }
+
+    String nextCursor = null;
+    LocalDateTime nextAfter = null;
+    switch (orderBy == null ? "" : orderBy) {
+      case "commentCount":
+        nextCursor = articles.size() == limit+1 ? String.valueOf(articles.get(limit).getArticleCommentCount()) : null;
+        nextAfter = articles.size() == limit+1 ? articles.get(limit).getCreatedAt() : null;
+        break;
+      case "viewCount":
+        nextCursor = articles.size() == limit+1 ? String.valueOf(articles.get(limit).getArticleViewCount()) : null;
+        nextAfter = articles.size() == limit+1 ? articles.get(limit).getCreatedAt() : null;
+        break;
+      case "publishDate":
+        nextCursor = articles.size() == limit+1 ? String.valueOf(articles.get(limit).getArticlePublishDate()) : null;
+        nextAfter = articles.size() == limit+1 ? articles.get(limit).getCreatedAt() : null;
+        break;
+    }
+    int size = articles.size();
+    long totalElements = articleRepository.searchArticlesCount(keyword, interestId, sourceIn, publishDateFrom, publishDateTo);
+    boolean hasNex = size == limit+1;
 
     log.info("뉴스 기사 검색 완료");
-    return null;
+    return new CursorPageResponseArticleDto(content, nextCursor, nextAfter, size, totalElements, hasNex);
   }
 
   private Article checkArticle(UUID articleId) {
