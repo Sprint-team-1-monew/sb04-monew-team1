79578a593cdd059338968893fa89346dfd00a1bd
Eunhye0k <dmsgur7305@naver.com>
2025-09-11 18:34:51 +0900

    feat(댓글 관리): @Valid 조건 DTO 에 추가

--- src/main/java/com/codeit/monew/comment/request/CommentUpdateRequest.java
+++ src/main/java/com/codeit/monew/comment/request/CommentUpdateRequest.java
@@ -1,5 +1,9 @@
 package com.codeit.monew.comment.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CommentUpdateRequest(
+
+  @NotNull(message = "수정 할 내용은 필수 입니다.")
   String content
 ){}
