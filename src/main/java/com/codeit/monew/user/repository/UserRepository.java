ae04cd90fbee4ff2dcaee59ded63d9edd6a2876f
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-09-12 16:20:50 +0900

    feature(사용자 관리) : 물리 삭제 기능 추가
    - 물리 삭제 성공 테스트 추가
    - 물리삭제 관련 에러코드 추가
    - 물리삭제 대상 조회 쿼리 메서드 추가
    - 서비스, 컨트롤러 계층 코드 작성
    - 물리삭제 스케줄러 추가

--- src/main/java/com/codeit/monew/user/repository/UserRepository.java
+++ src/main/java/com/codeit/monew/user/repository/UserRepository.java
@@ -2,11 +2,17 @@ package com.codeit.monew.user.repository;
 
 import com.codeit.monew.user.entity.User;
 import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByEmail(String email);
 
   Optional<User> findByEmail(String email);
+
+  @Query("SELECT u FROM User u WHERE u.userStatus = 'DELETED' AND u.deletedAt <= :now")
+  List<User> findUsersForHardDelete(LocalDateTime now);
 }
