b13338bb6f90767bee8a147d3b1e9940b8205243
KwonYongJin <chaoskyj@gmail.com>
2025-09-15 14:39:04 +0900

    fix(뉴스 기사 관리): 뉴스 기사 조회 수 및 댓글 수 정상 조회
    뉴스 기사 조회수 및 댓글 수 정상 조회되도록 반영

--- src/main/java/com/codeit/monew/comment/repository/CommentRepository.java
+++ src/main/java/com/codeit/monew/comment/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.codeit.monew.comment.repository;
 
+import com.codeit.monew.article.entity.Article;
 import com.codeit.monew.comment.entity.Comment;
 import java.util.Optional;
 import java.util.UUID;
@@ -9,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
   Optional<Comment> findById(UUID id);
+
+  int countByArticle(Article article);
 }
