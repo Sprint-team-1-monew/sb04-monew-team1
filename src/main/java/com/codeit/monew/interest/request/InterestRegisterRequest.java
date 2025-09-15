32da74e781ab49801a7e7f58a91374d99782cdbc
Oh-Myeongjae <omjcws@gmail.com>
2025-09-15 11:29:11 +0900

    feat(관심사 관리): 관심사 등록, 수정시 입력값 검증 구현
    관심사 등록, 수정시 입력값 검증 구현

--- src/main/java/com/codeit/monew/interest/request/InterestRegisterRequest.java
+++ src/main/java/com/codeit/monew/interest/request/InterestRegisterRequest.java
@@ -1,8 +1,18 @@
 package com.codeit.monew.interest.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record InterestRegisterRequest(
-  String name,
-  List<String> keywords
+    @NotBlank(message = "관심사 이름은 필수입니다")
+    @Size(min = 1, max = 50, message = "관심사 이름은 1자 이상 50자 이하여야 합니다")
+    String name,
+
+    @NotNull(message = "키워드 목록은 필수입니다")
+    @NotEmpty(message = "키워드는 최소 1개 이상 입력해야 합니다")
+    @Size(min = 1, max = 10, message = "키워드는 1개 이상 10개 이하여야 합니다")
+    List<@NotBlank(message = "키워드는 비어있을 수 없습니다") String> keywords
 ) {}
