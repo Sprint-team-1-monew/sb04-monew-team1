8a954bd04245b221fbdcca0ed2363d9313c8faaa
Oh-Myeongjae <omjcws@gmail.com>
2025-09-15 14:18:25 +0900

    feat(관심사 관리): 관심사 삭제시 구독한 유저의 구독취소 구현
    관심사 삭제시 구독한 유저의 구독취소 구현

--- src/main/java/com/codeit/monew/subscriptions/repository/SubscriptionRepository.java
+++ src/main/java/com/codeit/monew/subscriptions/repository/SubscriptionRepository.java
@@ -3,6 +3,7 @@ package com.codeit.monew.subscriptions.repository;
 import com.codeit.monew.interest.entity.Interest;
 import com.codeit.monew.subscriptions.entity.Subscription;
 import com.codeit.monew.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,5 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, UUID
 
   Optional<Subscription> findByUserAndInterest(User user, Interest interest);
 
+  List<Subscription> findAllByInterest(Interest interest);
 }
