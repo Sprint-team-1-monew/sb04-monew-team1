ebd9321357a91360adf44ea07b8eaea7da31a37b
Oh-Myeongjae <omjcws@gmail.com>
2025-09-15 14:36:17 +0900

    feat(관심사 관리): 관심사 삭제시 구독한 유저의 구독취소 구현
    관심사 삭제시 구독한 유저의 구독취소 구현

--- src/main/java/com/codeit/monew/interest/service/InterestService.java
+++ src/main/java/com/codeit/monew/interest/service/InterestService.java
@@ -153,8 +153,7 @@ public class InterestService {
     // 관련 키워드들도 소프트 삭제
     List<Keyword> activeKeywords = keywordRepository.findByInterestAndIsDeletedFalse(interest);
     activeKeywords.forEach(Keyword::softDelete);
-
-    // 해당 관심사를 구독한 모든 구독 정보 삭제
+    
     List<Subscription> subscriptions = subscriptionRepository.findAllByInterest(interest);
     subscriptionRepository.deleteAll(subscriptions);
 
