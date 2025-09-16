be8fd9cc9dee467977fe77482a6bd5a76ae064d2
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-09-16 18:50:20 +0900

    [뉴스 기사 관리] 뉴스 기사 데이터를 S3에 백업하는 기능 구현  - 날짜 단위로 백업하기  - 백업 저장소는 AWS S3를 활용하기  - 백업 작업은 배치로 수행하기, @Scheduled 사용

--- src/main/java/com/codeit/monew/article/backUp/dto/ArticleBackupDto.java
+++ src/main/java/com/codeit/monew/article/backUp/dto/ArticleBackupDto.java
@@ -1,4 +1,4 @@
-package com.codeit.monew.article.backUp;
+package com.codeit.monew.article.backUp.dto;
 
 import com.codeit.monew.article.entity.Article;
 
@@ -13,7 +13,7 @@ public record ArticleBackupDto(
         long articleCommentCount,
         long articleViewCount,
         boolean deleted,
-        String interestId // 복구할 때 필요함
+        String interestId // 복구할 때 필요함, 관심사 ID를 가져옴
 ) {
     public static ArticleBackupDto from(Article article) {
         String interestIdStr = (article.getInterest() != null && article.getInterest().getId() != null) // Interest가 Lazy해도 ID접근을 프록시로 가능하게끔
