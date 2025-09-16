6604b56d0c9fed2308b7952ee7d4993b839c4c47
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-09-17 00:58:28 +0900

    [뉴스 기사 관리] 뉴스 기사 데이터를 S3에 백업하는 기능 구현 수정  - 날짜 단위로 백업하기  - 백업 저장소는 AWS S3를 활용하기  - 백업 작업은 배치로 수행하기, @Scheduled 사용
     * @Value값을 Git Secret으로 형상관리하게끔 구현은 불가능, @Value가 인식하지 못함
     * application.yaml에서 백업관련 형상관리 처리
     * Dto에서 필요없는 조건문 주석처리

--- src/main/java/com/codeit/monew/article/backUp/aws/S3Config.java
+++ src/main/java/com/codeit/monew/article/backUp/aws/S3Config.java
@@ -8,10 +8,8 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-
     @Value("${aws.s3.region}")
     private String region;
-
     @Bean
     public S3Client s3Client() {
         return S3Client.builder().region(Region.AP_NORTHEAST_2).build();
