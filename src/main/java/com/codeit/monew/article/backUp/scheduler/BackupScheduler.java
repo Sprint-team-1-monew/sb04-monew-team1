66bae26c270005fabaf76cbe5743d6b5392a4f1f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-09-16 15:13:42 +0900

    [뉴스 기사 관리] 뉴스 기사 데이터를 S3에 백업하는 기능 구현  - 날짜 단위로 백업하기  - 백업 저장소는 AWS S3를 활용하기  - 백업 작업은 배치로 수행하기, @Scheduled 사용
    # Conflicts:
    #	src/main/resources/application-prod.yaml
    
    # Conflicts:
    #	build.gradle

---
+++ src/main/java/com/codeit/monew/article/backUp/scheduler/BackupScheduler.java
@@ -0,0 +1,27 @@
+package com.codeit.monew.article.backUp.scheduler;
+
+import com.codeit.monew.article.backUp.service.ArticleBackupService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BackupScheduler {
+    private final ArticleBackupService backupService;
+
+    // 초, 분, 시, 일, 월, 요일
+    // 매일 0시, 전날의 데이터를 백업함
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    public void backupArticle() {
+        LocalDate target = LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1);
+        log.info("뉴스기사 백업 시작, {}", target);
+        backupService.articleBackup(target);
+    }
+}
