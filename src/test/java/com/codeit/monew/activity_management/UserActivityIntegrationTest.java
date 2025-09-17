bbd35be362f23fd591968a60d4d2f861877a64b2
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-09-17 13:12:05 +0900

    feature(사용자 관리) : 활동 내역 관리 테스트 코드 sql 파일 실행 시점 수정
    - 테스트 메서드마다 -> 클래스 로딩 시점

--- src/test/java/com/codeit/monew/activity_management/UserActivityIntegrationTest.java
+++ src/test/java/com/codeit/monew/activity_management/UserActivityIntegrationTest.java
@@ -18,7 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
-@Sql("/activity-test-data.sql")
+@Sql(scripts ="/activity-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 @SpringBootTest
 public class UserActivityIntegrationTest {
 
