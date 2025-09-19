cbd95edd387fb5c557d07b08b493026b6e9666c8
KwonYongJin <chaoskyj@gmail.com>
2025-09-19 18:48:13 +0900

    style(뉴스 기사 관리): 컨벤션에 맞게 수정

--- src/main/java/com/codeit/monew/article/rss/RssItem.java
+++ src/main/java/com/codeit/monew/article/rss/RssItem.java
@@ -2,4 +2,9 @@ package com.codeit.monew.article.rss;
 
 import java.time.LocalDateTime;
 
-public record RssItem(String title, String link, LocalDateTime publishedAt) {}
+public record RssItem(
+    String title,
+    String link,
+    LocalDateTime publishedAt
+) {}
+
